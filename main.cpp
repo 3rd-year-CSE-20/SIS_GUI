5a79bd212a2af820f9c217bdad20b1fdc2eff160
alien19 <engmariaam@outlook.com>
2019-12-24 01:18:15 +0000

    edited main

--- main.cpp
+++ main.cpp
@@ -7,16 +7,13 @@
 #define ELECTRICAL_DEPARTMENT 1
 int main(int argc, char *argv[])
 {
-     //databaseInitialization(); //uncomment if it is the first time to run the code
+     databaseInitialization(); //uncomment if it is the first time to run the code
     QApplication a(argc, argv);
-    Admin mariam;
-    mariam.setId(1);
-    mariam.setFirstName("Mariam");
-    mariam.setPassword("123");
-    mariam.setCollegeId("1600001");
+    Admin mariam ("Mariam", "Ali", "Female", "2","4/6/1998", " ",
+            "1600001", "123");
 
     mariam.save();
-    qDebug()<<mariam.getCollegeId();
+    //qDebug()<<mariam.getCollegeId();
     MainWindow w;
 
     w.show();
