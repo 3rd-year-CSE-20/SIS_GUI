c193c5b614b56f8b416fff8b9e4205272a5e22ca
alien19 <engmariaam@outlook.com>
2019-12-31 20:39:15 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,6 @@ int main(int argc, char *argv[]){
 //    databaseInitialization(); //uncomment if it is the first time to run the code
     QApplication a(argc, argv);
     MainWindow w;
-    w.showFullScreen();
+    w.show();
     return a.exec();
 }
