e00065e4a561065103281a6d16140c51df4aacc1
theMeanScore <m.ad.ali2165@gmail.com>
2019-12-31 16:46:18 +0200

    Fix addCourse() and deleteCourse()

--- staffmember.cpp
+++ staffmember.cpp
@@ -15,77 +15,88 @@
 #include <globalDbObject.h>
 
 static QString staff_table = "staff_members";
-static QStringList staff_columns = {"first_name","last_name", "degree", "birth_date",
-                                "gendre", "address", "password", "picture", "degree", "department"};
+static QStringList staff_columns = {"first_name", "last_name", "degree", "birth_date",
+                                    "gendre", "address", "password", "picture", "degree", "department"};
 static QStringList staff_types = {"INTEGER PRIMARY KEY AUTOINCREMENT", "TEXT", "TEXT", "TEXT", "TEXT",
-                              "TEXT", "TEXT", "TEXT", "TEXT", "TEXT", "TEXT"};
+                                  "TEXT", "TEXT", "TEXT", "TEXT", "TEXT", "TEXT"};
 
-StaffMember::StaffMember() : Person() {
+StaffMember::StaffMember() : Person()
+{
     this->degree = QString("");
     this->department = QString("");
     this->courses = QVector<Course>();
 }
 
 StaffMember::StaffMember(QString first_name, QString last_name, QString gendre, QString picture, QString birth_date, QString address, QString college_id, QString password, QString degree, QString department)
-    : Person(first_name, last_name, gendre, picture, birth_date, address, college_id, password) {
+    : Person(first_name, last_name, gendre, picture, birth_date, address, college_id, password)
+{
     this->degree = degree;
     this->department = department;
     this->courses = QVector<Course>();
 }
 
-void StaffMember::setDegree(QString degree) {
+void StaffMember::setDegree(QString degree)
+{
     this->degree = degree;
 }
 
-QString StaffMember::getDegree() {
+QString StaffMember::getDegree()
+{
     return this->degree;
 }
 
-void StaffMember::setDepartment(QString department) {
+void StaffMember::setDepartment(QString department)
+{
     this->department = department;
 }
 
-QString StaffMember::getDepartment() {
+QString StaffMember::getDepartment()
+{
     return this->department;
 }
 
-
-QVector<Course> StaffMember::getCourses() {
+QVector<Course> StaffMember::getCourses()
+{
     return this->courses;
 }
 
-void StaffMember::addCourse(QString course_name) {
+void StaffMember::addCourse(QString course_name)
+{
     int index = -1;
-    for(int i = 0; i < this->courses.size(); i++) {
-        if(this->courses[0].getName() == course_name) {
+    for (int i = 0; i < this->courses.size(); i++)
+    {
+        if (this->courses[i].getName() == course_name)
+        {
             index = i;
             break;
         }
     }
-    if(index == -1)
+    if (index == -1)
         this->courses.push_back(Course(course_name));
 }
 
-void StaffMember::deleteCourse(QString course_name) {
-    int index = -1;
-    for(int i = 0; i < this->courses.size(); i++) {
-        if(this->courses[0].getName() == course_name) {
-            index = i;
+void StaffMember::deleteCourse(QString course_name)
+{
+    for (int i = 0; i < this->courses.size(); i++)
+    {
+        if (this->courses[i].getName() == course_name)
+        {
+            this->courses.remove(i);
             break;
         }
     }
-    if(index != -1)
-        this->courses.remove(index);
 }
 
-QVector<StaffMember> StaffMember::all() {
+QVector<StaffMember> StaffMember::all()
+{
     QSqlQuery query = SQLiteDb.sql_getQuery();
 
     QVector<StaffMember> staff_members;
     StaffMember temp;
     query.exec("SELECT * FROM staff_members");
     QSqlQuery query1;
-    while(query.next()) {
+    while (query.next())
+    {
         long long id = query.value(0).toLongLong();
         QString first_name = query.value(1).toString();
         QString last_name = query.value(2).toString();
@@ -103,7 +114,8 @@ QVector<StaffMember> StaffMember::all() {
         temp.setIsSaved(true);
 
         query1.exec("SELECT * FROM courses_staff_members WHERE course_id = " + QString::number(id));
-        while(query1.next()) {
+        while (query1.next())
+        {
             temp.addCourse(Course::find(query1.value(1).toLongLong()).getName());
         }
         staff_members.push_back(temp);
@@ -112,18 +124,21 @@ QVector<StaffMember> StaffMember::all() {
     return staff_members;
 }
 
-bool StaffMember::isInDatabase(long long id) {
+bool StaffMember::isInDatabase(long long id)
+{
     QSqlQuery query = SQLiteDb.sql_getQuery();
 
     query.exec("SELECT * FROM staff_members WHERE id = " + QString::number(id));
-    if(query.next()) {
+    if (query.next())
+    {
         return true;
     }
 
     return false;
 }
 
-StaffMember StaffMember::find(long long id) {
+StaffMember StaffMember::find(long long id)
+{
     QSqlQuery query = SQLiteDb.sql_getQuery();
 
     query.exec("SELECT * FROM staff_members WHERE id = " + QString::number(id));
@@ -145,55 +160,62 @@ StaffMember StaffMember::find(long long id) {
     staff_member.setIsSaved(true);
 
     query1.exec("SELECT * FROM courses_staff_members WHERE course_id = " + QString::number(id));
-    while(query1.next()) {
+    while (query1.next())
+    {
         staff_member.addCourse(Course::find(query1.value(1).toLongLong()).getName());
     }
 
     return staff_member;
 }
 
-bool StaffMember::save(){
+bool StaffMember::save()
+{
     QString id_ = QString::number(getId());
     SQLiteDb.sql_select("*", staff_table, " id = " + id_);
     QSqlQuery query = SQLiteDb.sql_getQuery();
-    QStringList values = {getFirstName(),  getLastName(), getDegree(), getBirthDate(),
-                          getGendre(), getAddress(), getPassword(), getPicture(), getDegree(),  getDepartment()};
-    if(query.next()){
+    QStringList values = {getFirstName(), getLastName(), getDegree(), getBirthDate(),
+                          getGendre(), getAddress(), getPassword(), getPicture(), getDegree(), getDepartment()};
+    if (query.next())
+    {
         SQLiteDb.sql_update(staff_table, staff_columns, values, "id = " + id_);
 
-        for(int i = 0; i < courses.size(); i++){
+        for (int i = 0; i < courses.size(); i++)
+        {
             QString course_id = QString::number(courses[i].getId());
             SQLiteDb.sql_select("*", "courses_staff_members", " staff_member_id = " + id_ + " AND course_id " + course_id);
             query = SQLiteDb.sql_getQuery();
-            if(!query.next()){
+            if (!query.next())
+            {
                 SQLiteDb.sql_insert("courses_staff_members", {"staff_member_id", "courses_id"}, {id_, course_id});
             }
         }
         return true;
     }
     SQLiteDb.sql_insert(staff_table, staff_columns, values);
-    for(int i = 0; i < courses.size(); i++){
+    for (int i = 0; i < courses.size(); i++)
+    {
         QString course_id = QString::number(courses[i].getId());
         SQLiteDb.sql_insert("courses_staff_members", {"staff_member_id", "course_id"}, {id_, course_id});
     }
     return false;
 }
 
-QVector<StaffMember> StaffMember::where(QString column, QString value){
-    SQLiteDb.sql_select("*", staff_table, column + " = " +  value);
+QVector<StaffMember> StaffMember::where(QString column, QString value)
+{
+    SQLiteDb.sql_select("*", staff_table, column + " = " + value);
     QSqlQuery query = SQLiteDb.sql_getQuery();
     QVector<StaffMember> staff_members;
-    while (query.next()) {
+    while (query.next())
+    {
         int id = query.value(0).toInt();
         staff_members.push_back(find(id));
     }
     return staff_members;
 }
 
-void StaffMember::delete1(){
+void StaffMember::delete1()
+{
     QString staff_id = QString::number(getId());
     SQLiteDb.sql_delete(staff_table, "id = " + staff_id);
     SQLiteDb.sql_delete("courses_staff_members", "staff_member_id = " + staff_id);
 }
-
-
--- student.cpp
+++ student.cpp
@@ -3,8 +3,8 @@
 #include "globalDbObject.h"
 
 static QString students_table = "students";
-static QStringList students_columns = {"first_name","last_name", "gendre", "picture",
-                                "birth_date", "address", "college_id", "password", "academic_year", "department"};
+static QStringList students_columns = {"first_name", "last_name", "gendre", "picture",
+                                       "birth_date", "address", "college_id", "password", "academic_year", "department"};
 
 Student::Student() : Person()
 {
@@ -13,71 +13,84 @@ Student::Student() : Person()
 }
 
 Student::Student(QString first_name, QString last_name, QString gendre, QString picture, QString birth_date, QString address, QString college_id, QString password,
-                 QString academic_year, QString department) : Person(first_name, last_name, gendre, picture, birth_date, address, college_id, password) {
-    this-> academic_year = academic_year;
+                 QString academic_year, QString department) : Person(first_name, last_name, gendre, picture, birth_date, address, college_id, password)
+{
+    this->academic_year = academic_year;
     this->department = department;
 }
 
-void Student::setAcademicYear(QString academic_year){
+void Student::setAcademicYear(QString academic_year)
+{
     this->academic_year = academic_year;
 }
 
-void Student::setDepartment(QString department) {
+void Student::setDepartment(QString department)
+{
     this->department = department;
 }
 
-QString Student::getDepartment() {
+QString Student::getDepartment()
+{
     return this->department;
 }
 
-QString Student::getAcademicYear(){
-    return this-> academic_year;
+QString Student::getAcademicYear()
+{
+    return this->academic_year;
 }
 
-QVector<Course> Student::getCourses() {
+QVector<Course> Student::getCourses()
+{
     return this->courses;
 }
 
-void Student::setCollegeId(QString college_id){
+void Student::setCollegeId(QString college_id)
+{
     this->college_id = college_id;
 }
 
-QString Student::getCollegeId(){
+QString Student::getCollegeId()
+{
     return this->college_id;
 }
 
-void Student::addCourse(QString course_name) {
+void Student::addCourse(QString course_name)
+{
     int index = -1;
-    for(int i = 0; i < this->courses.size(); i++) {
-        if(this->courses[0].getName() == course_name) {
+    for (int i = 0; i < this->courses.size(); i++)
+    {
+        if (this->courses[i].getName() == course_name)
+        {
             index = i;
             break;
         }
     }
-    if(index == -1)
+    if (index == -1)
         this->courses.push_back(Course(course_name));
 }
 
-void Student::deleteCourse(QString course_name) {
-    int index = -1;
-    for(int i = 0; i < this->courses.size(); i++) {
-        if(this->courses[0].getName() == course_name) {
-            index = i;
+void Student::deleteCourse(QString course_name)
+{
+    for (int i = 0; i < this->courses.size(); i++)
+    {
+        if (this->courses[i].getName() == course_name)
+        {
+            this->courses.remove(i);
             break;
         }
     }
-    if(index != -1)
-        this->courses.remove(index);
 }
 
-QVector<Student> Student::all() {
+QVector<Student> Student::all()
+{
     QSqlQuery query = SQLiteDb.sql_getQuery();
 
     QVector<Student> students;
     Student temp;
     query.exec("SELECT * FROM students");
     QSqlQuery query1;
-    while(query.next()) {
+    while (query.next())
+    {
         long long id = query.value(0).toLongLong();
         QString first_name = query.value(1).toString();
         QString last_name = query.value(2).toString();
@@ -90,13 +103,13 @@ QVector<Student> Student::all() {
         QString academic_year = query.value(9).toString();
         QString department = query.value(10).toString();
 
-
         Student temp(first_name, last_name, gendre, picture, birth_date, address, college_id, password, academic_year, department);
         temp.setId(id);
         temp.setIsSaved(true);
 
         query1.exec("SELECT * FROM courses_students WHERE course_id = " + QString::number(id));
-        while(query1.next()) {
+        while (query1.next())
+        {
             temp.addCourse(Course::find(query1.value(1).toLongLong()).getName());
         }
         students.push_back(temp);
@@ -105,51 +118,60 @@ QVector<Student> Student::all() {
     return students;
 }
 
-bool Student::isInDatabase(long long id) {
+bool Student::isInDatabase(long long id)
+{
     QSqlQuery query = SQLiteDb.sql_getQuery();
 
     query.exec("SELECT * FROM students WHERE id = " + QString::number(id));
-    if(query.next()) {
+    if (query.next())
+    {
         return true;
     }
 
     return false;
 }
 
-bool Student::save(){
+bool Student::save()
+{
     QString id_ = QString::number(getId());
     SQLiteDb.sql_select("*", students_table, " id = " + id_);
     QSqlQuery query = SQLiteDb.sql_getQuery();
-    QStringList values = {getFirstName(),  getLastName(), getGendre(), getPicture(),
-                          getBirthDate(), getAddress(), getCollegeId(), getPassword(), getAcademicYear(),  getDepartment()};
-    if(query.next()){
+    QStringList values = {getFirstName(), getLastName(), getGendre(), getPicture(),
+                          getBirthDate(), getAddress(), getCollegeId(), getPassword(), getAcademicYear(), getDepartment()};
+    if (query.next())
+    {
         SQLiteDb.sql_update(students_table, students_columns, values, "id = " + id_);
 
-        for(int i = 0; i < courses.size(); i++){
+        for (int i = 0; i < courses.size(); i++)
+        {
             QString course_id = QString::number(courses[i].getId());
             SQLiteDb.sql_select("*", "courses_students", " student_id = " + id_ + " AND course_id " + course_id);
             query = SQLiteDb.sql_getQuery();
-            if(!query.next()){
+            if (!query.next())
+            {
                 SQLiteDb.sql_insert("courses_students", {"student_id", "course_id"}, {id_, course_id});
             }
         }
         return true;
     }
     SQLiteDb.sql_insert(students_table, students_columns, values);
-    for(int i = 0; i < courses.size(); i++){
+    for (int i = 0; i < courses.size(); i++)
+    {
         QString course_id = QString::number(courses[i].getId());
         SQLiteDb.sql_insert("courses_students", {"student_id", "course_id"}, {id_, course_id});
     }
     return false;
 }
 
-void Student::delete1(){
+void Student::delete1()
+{
     QString student_id = QString::number(getId());
     SQLiteDb.sql_delete(students_table, "id = " + student_id);
     SQLiteDb.sql_delete("courses_students", "student_id = " + student_id);
 }
 
-Student Student::find(long long id) {
+Student Student::find(long long id)
+{
     QSqlQuery query = SQLiteDb.sql_getQuery();
 
     query.exec("SELECT * FROM students WHERE id = " + QString::number(id));
@@ -171,25 +193,23 @@ Student Student::find(long long id) {
     student.setIsSaved(true);
 
     query1.exec("SELECT * FROM courses_students WHERE course_id = " + QString::number(id));
-    while(query1.next()) {
+    while (query1.next())
+    {
         student.addCourse(Course::find(query1.value(1).toLongLong()).getName());
     }
 
     return student;
 }
 
-QVector<Student> Student::where(QString column, QString value){
-    SQLiteDb.sql_select("*", students_table, column + " = " +  value);
+QVector<Student> Student::where(QString column, QString value)
+{
+    SQLiteDb.sql_select("*", students_table, column + " = " + value);
     QSqlQuery query = SQLiteDb.sql_getQuery();
     QVector<Student> students;
-    while (query.next()) {
+    while (query.next())
+    {
         int id = query.value(0).toInt();
         students.push_back(find(id));
     }
     return students;
 }
-
-
-
-
-
